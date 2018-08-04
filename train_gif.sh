1f13c4c43de171258606d6ddbf68c842e903a8ba
Ubuntu <ubuntu@ip-10-30-60-222.ec2.internal>
2018-08-04 18:37:57 +0000

    added aha source

--- train_gif.sh
+++ train_gif.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-time python train.py --train_dir ./data/formatted                 --style_img_path ./style_images/nothing_to_see/nothing_to_see001.png                 --model_name nothing_1                 --n_epochs 2                 --batch_size 4                 --content_weights 0.5                 --style_weights 5.0 5.0 5.0 5.0                 --style_target_resize 0.5 
-
-time python train.py --train_dir ./data/formatted                 --style_img_path ./style_images/nothing_to_see/nothing_to_see010.png                 --model_name nothing_2                 --n_epochs 2                 --batch_size 4                 --content_weights 0.5                 --style_weights 5.0 5.0 5.0 5.0                 --style_target_resize 0.5 
-
-time python train.py --train_dir ./data/formatted                 --style_img_path ./style_images/nothing_to_see/nothing_to_see020.png                 --model_name nothing_3                 --n_epochs 2                 --batch_size 4                 --content_weights 0.5                 --style_weights 5.0 5.0 5.0 5.0                 --style_target_resize 0.5 
-
-time python train.py --train_dir ./data/formatted                 --style_img_path ./style_images/nothing_to_see/nothing_to_see030.png                 --model_name nothing_4                 --n_epochs 2                 --batch_size 4                 --content_weights 0.5                 --style_weights 5.0 5.0 5.0 5.0                 --style_target_resize 0.5 
-
-time python train.py --train_dir ./data/formatted                 --style_img_path ./style_images/nothing_to_see/nothing_to_see040.png                 --model_name nothing_5                 --n_epochs 2                 --batch_size 4                 --content_weights 0.5                 --style_weights 5.0 5.0 5.0 5.0                 --style_target_resize 0.5 
-
+time python train.py --train_dir ./data/formatted                 --style_img_path ./style_images/aha/aha001.png                 --model_name aha_1                 --n_epochs 2                 --batch_size 4                 --content_weights 0.5                 --style_weights 5.0 5.0 5.0 5.0                 --style_target_resize 0.5
+time python train.py --train_dir ./data/formatted                 --style_img_path ./style_images/aha/aha0015.png                 --model_name aha_2                 --n_epochs 2                 --batch_size 4                 --content_weights 0.5                 --style_weights 5.0 5.0 5.0 5.0                 --style_target_resize 0.5
+time python train.py --train_dir ./data/formatted                 --style_img_path ./style_images/aha/aha0030.png                 --model_name aha_3                 --n_epochs 2                 --batch_size 4                 --content_weights 0.5                 --style_weights 5.0 5.0 5.0 5.0                 --style_target_resize 0.5 
 
 
